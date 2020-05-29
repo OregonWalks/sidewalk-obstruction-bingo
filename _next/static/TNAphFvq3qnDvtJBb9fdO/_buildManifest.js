97bd055c8c4fe38b67405424e240511119a54774
jyasskin <jyasskin@users.noreply.github.com>
2020-05-29 04:03:02 +0000

    deploy: 1593bd02116a9d293fddece649ea056ac270530b

---
+++ _next/static/TNAphFvq3qnDvtJBb9fdO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F05ca7a2cd21e38c6e72d.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/gSZJF69yRfuQv8xJHcNJI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002Ff97293389b4f686bf4db.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
